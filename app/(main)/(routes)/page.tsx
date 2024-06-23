eb8e08551bbdd12f355db83adbcd62692e2d1d76
“Slim” <1774302604@qq.com>
2024-06-23 18:06:53 +0800

    add clerk signIn signUp and theme

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      123
+      <UserButton afterSignOutUrl="/"></UserButton>
+    </div>
+  );
+}
